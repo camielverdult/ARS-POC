4f217b373f78e3aa5658469bdd7aa8fe0384cb62
Elitsa Marinova <elimarinova123@gmail.com>
2024-01-09 16:49:31 +0100

    Add request function.

--- get_labels.py
+++ get_labels.py
@@ -1,9 +1,24 @@
 import db
 import requests
+import json
 
 def get_labels():
     # Get the succesful domains for building a query to the labelling API
     successful_domains = db.get_successful_domains()
 
-    # Turn succesful domain URLs into a GET request to the labelling API
-    
+    # Turn successful domain URLs into a GET request to the labeling API
+
+    url = "https://api-url.cyren.com/api/v1/free/urls-list"
+
+    payload = json.dumps({
+        "urls": successful_domains
+    })
+
+    headers = {
+    'Authorization': 'Bearer your.jwt.token',
+    'Content-Type': 'application/json'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    print(response.text)
