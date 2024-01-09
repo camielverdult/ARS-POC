966ba648bd23e78cb12c6975c7037b32ffc7ae64
Camiel Verdult <cpg@verdult.com>
2024-01-09 16:22:56 +0100

    Extract training_data functionality into distinct functions

--- get_labels.py
+++ get_labels.py
@@ -3,7 +3,7 @@ import requests
 
 def get_labels():
     # Get the succesful domains for building a query to the labelling API
-    successful_domains = db.get_successful_domains()
+    successful_domains = db.get_succesful_domain_urls()
 
     # Turn succesful domain URLs into a GET request to the labelling API
     
